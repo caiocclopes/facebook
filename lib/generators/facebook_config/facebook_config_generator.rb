eb26a3a90e1cbab29ab2fa207cff20d250dba83f
Bruno Nigro <bruno.nigro@gmail.com>
2011-04-29 10:03:25 -0300

    Facebook gem

---
+++ lib/generators/facebook_config/facebook_config_generator.rb
@@ -0,0 +1,16 @@
+# Use this generator like this:
+# rails g facebook_config
+
+class FacebookConfigGenerator < Rails::Generators::Base
+
+  def self.source_root
+    @source_root ||= File.join(File.dirname(__FILE__), 'templates')
+  end
+
+  def generate_scaffold
+    generate("scaffold", "facebook_config account_name:string priority:integer area_id:integer")
+    remove_file "./app/models/facebook_config.rb"
+    template "facebook_config_model.rb", "./app/models/facebook_config.rb"
+  end
+
+end
---
+++ lib/generators/facebook_config/templates/facebook_config_model.rb
@@ -0,0 +1,4 @@
+class FacebookConfig < Facebook::Model::FacebookModel
+  ##Place any application specific fields in here
+  #example: field :name, type: String
+end
