20d4ea2f6d4e4eb596f49da37279af86cb8ae8c1
Bruno Nigro <bruno.nigro@gmail.com>
2011-05-02 09:26:12 -0300

    Inserção do campo account_id

--- lib/facebook/model.rb
+++ lib/facebook/model.rb
@@ -4,6 +4,7 @@ module Facebook
     class FacebookModel
       include Mongoid::Document
       field :account_name
+      field :account_id
       field :priority, type: Integer
       field :area_id, type: Integer
       #Check what other fields need to be added here
